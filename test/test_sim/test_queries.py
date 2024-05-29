559849b471565dd27b473b873906a7b39af6c7b9
jrogoz <joanna.rogoz33@gmail.com>
2024-05-29 16:57:50 +0200

    Switch into pytest: test_queries.py

--- test/test_sim/test_queries.py
+++ test/test_sim/test_queries.py
@@ -1,56 +1,43 @@
-import unittest
 from residents.models import Sim
 from residents.queries import ListSimsQuery, GetSimByIDQuery
 
 
-class TestListSimsQuery(unittest.TestCase):
+def test_list_sims():
+    """
+    GIVEN 2 sims stored in the database
+    WHEN the execute method is called
+    THEN it should return 2 sims
+    """
+    Sim(
+        first_name='Brandi',
+        last_name='Broke',
+        is_alive=False
+    ).save()
+    Sim(
+        first_name='Don',
+        last_name='Lothario',
+        is_alive=False
+    ).save()
 
-    @classmethod
-    def setUpClass(cls):
-        Sim.create_table()
+    query = ListSimsQuery()
 
-    @classmethod
-    def tearDownClass(self):
-        Sim.delete_table()
+    assert len(query.execute()) == 2
 
-    def tearDown(self):
-        Sim.delete_sims()
 
-    def test_list_sims(self):
-        """
-        GIVEN 2 sims stored in the database
-        WHEN the execute method is called
-        THEN it should return 2 sims
-        """
-        Sim(
-            first_name='Brandi',
-            last_name='Broke',
-            is_alive=False
-        ).save()
-        Sim(
-            first_name='Don',
-            last_name='Lothario',
-            is_alive=False
-        ).save()
+def test_get_sim_by_id():
+    """
+    GIVEN ID of sim in the database
+    WHEN the executed method is called on GetSimByIDQuery with an ID
+    THEN id should return the sim with the same ID
+    """
+    sim = Sim(
+        first_name='Nina',
+        last_name='Caliente',
+        is_alive=True
+    ).save()
 
-        query = ListSimsQuery()
+    query = GetSimByIDQuery(
+        id=sim.id
+    )
 
-        self.assertEqual(len(query.execute()), 2)
-
-    def test_get_sim_by_id(self):
-        """
-        GIVEN ID of sim in the database
-        WHEN the executed method is called on GetSimByIDQuery with an ID
-        THEN id should return the sim with the same ID
-        """
-        sim = Sim(
-            first_name='Nina',
-            last_name='Caliente',
-            is_alive=True
-        ).save()
-
-        query = GetSimByIDQuery(
-            id=sim.id
-        )
-
-        self.assertEqual(query.execute().id, sim.id)
+    assert query.execute().id == sim.id
