1d6dd4ef913a51d1375b2df4fff6338f7f8d7122
jrogoz <joanna.rogoz33@gmail.com>
2024-07-05 14:32:09 +0200

    CRUD: Add create_sim

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -11,3 +11,11 @@ def get_sim(db: Session, sim_id: int):
 def get_sims(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Sim).offset(skip).limit(limit).all()
 
+
+def create_sim(db: Session, sim: schemas.SimCreate):
+    db_sim = models.Sim(**sim.model_dump())
+    db.add(db_sim)
+    db.commit()
+    db.refresh(db_sim)
+    return db_sim
+
